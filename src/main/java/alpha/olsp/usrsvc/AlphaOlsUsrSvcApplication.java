c5dd94faae722943667d6824a3f75572db967b11
Hein Htet Zaw <heinhtet.zaw.career@gmail.com>
2024-11-01 11:41:05 -0500

    init usr svc

---
+++ src/main/java/alpha/olsp/usrsvc/AlphaOlsUsrSvcApplication.java
@@ -0,0 +1,13 @@
+package alpha.olsp.usrsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaOlsUsrSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaOlsUsrSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alpha/olsp/usrsvc/AlphaOlsUsrSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package alpha.olsp.usrsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaOlsUsrSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
